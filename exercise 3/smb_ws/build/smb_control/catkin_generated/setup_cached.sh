ddb717b8aa098bb3e9fb0e8fe3ebb0c5da638267
Kumar Kanishk Singh <kksingh21@iitk.ac.in>
2022-10-25 20:40:07 +0530

    Added Files

---
+++ exercise 3/smb_ws/build/smb_control/catkin_generated/setup_cached.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/kanishk/Ws/smb_ws/devel/.private/smb_control:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/kanishk/Ws/smb_ws/devel/.private/smb_control/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/kanishk/Ws/smb_ws/devel/.private/smb_control/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PWD='/home/kanishk/Ws/smb_ws/build/smb_control'
+export ROSLISP_PACKAGE_DIRECTORIES="/home/kanishk/Ws/smb_ws/devel/.private/smb_control/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/kanishk/Ws/smb_ws/src/smb_common/smb_control:/home/kanishk/Ws/smb_ws/src/smb_common_v2/smb_control:$ROS_PACKAGE_PATH"
---
+++ exercise 3/smb_ws/src/smb_common_v2/smb_highlevel_controller/src/smb_highlevel_controller_node.cpp
@@ -0,0 +1,123 @@
+// #include<algorithm>
+#include <ros/ros.h>
+#include "smb_highlevel_controller/SmbHighlevelController.hpp"
+#include <std_msgs/String.h>
+#include<sensor_msgs/LaserScan.h>
+#include <geometry_msgs/Twist.h>
+#include<cmath>
+using namespace std;
+// #include<sensor_msgs.msg/LaserScan>
+
+class controller
+{
+  public:
+    controller()
+    {
+      subscriber = nodeHandle.subscribe("/scan", 50, scanCallback);
+      publisher = nodeHandle.advertise<geometry_msgs::Twist>("/cmd_vel", 50);
+      // smb_highlevel_controller::SmbHighlevelController smbHighlevelController(nodeHandle);
+
+    }
+    void scanCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
+{
+
+
+double angle=0;
+double rangeMin=100000;
+for(double i=0;i<=720;i=i+1)
+{
+  if(msg->ranges[i] <= rangeMin)
+  {
+    rangeMin = msg->ranges[i];
+    angle = i;
+  }
+}
+ROS_INFO("Range: [%f]", msg->range_min);
+ROS_INFO("Angle: [%f]", angle);
+angle = angle*3.14/180;
+double xrel = rangeMin*cos(angle);
+double yrel = rangeMin*sin(angle);
+geometry_msgs::Twist move;
+while(msg->range_min>0)
+{
+  if(angle>0)
+  {
+    move.angular.x=move.angular.x+0.1;
+  }
+  if(xrel>0)
+  {
+    move.linear.x=10;
+  }
+  publisher.publish(move);
+}
+
+}
+
+// void scanCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
+// {
+
+
+// double angle=0;
+// double rangeMin=100000;
+// for(double i=0;i<=720;i=i+1)
+// {
+//   if(msg->ranges[i] <= rangeMin)
+//   {
+//     rangeMin = msg->ranges[i];
+//     angle = i;
+//   }
+// }
+// ROS_INFO("Range: [%f]", msg->range_min);
+// ROS_INFO("Angle: [%f]", angle);
+// angle = angle*3.14/180;
+// double xrel = rangeMin*cos(angle);
+// double yrel = rangeMin*sin(angle);
+// geometry_msgs::Twist move;
+// while(msg->range_min>0)
+// {
+//   if(angle>0)
+//   {
+//     move.angular.x=move.angular.x+0.1;
+//   }
+//   if(xrel>0)
+//   {
+//     move.linear.x=10;
+//   }
+// }
+private:
+ros::NodeHandle nodeHandle("~");
+ros::Subscriber subscriber;
+ros::Publisher publisher;
+smb_highlevel_controller::SmbHighlevelController smbHighlevelController(nodeHandle);
+
+
+
+}
+
+int main(int argc, char** argv) {
+  
+  ros::init(argc, argv, "smb_highlevel_controller");
+  controller cont;
+  // ros::NodeHandle nodeHandle("~");
+
+  // ros::Subscriber subscriber = nodeHandle.subscribe("/scan", 50, scanCallback);
+  // ros::Publisher publisher = nodeHandle.advertise<geometry_msgs::Twist>("/cmd_vel", 50);
+  // ros::Rate loop_rate(10);
+
+  // int count = 0;
+  // while (ros::ok())
+  // { geometry_msgs::Twist move;
+  //   move.linear.x=10;
+  //   publisher.publish(move);
+  //   ros::spinOnce();
+  //   loop_rate.sleep();
+  //   ++count;
+  // }
+
+  
+
+  // smb_highlevel_controller::SmbHighlevelController smbHighlevelController(nodeHandle);
+
+  ros::spin();
+  return 0;
+}
