02be75a574f58310ca3a2c5984ea98f04d31c11d
Kumar Kanishk Singh <kksingh21@iitk.ac.in>
2022-10-25 23:42:17 +0530

    Added Files

--- exercise 3/smb_ws/src/smb_common_v2/smb_highlevel_controller/src/smb_highlevel_controller_node.cpp
+++ exercise 3/smb_ws/src/smb_common_v2/smb_highlevel_controller/src/smb_highlevel_controller_node.cpp
@@ -56,13 +56,15 @@ using namespace std;
 // ros::NodeHandle nodeHandle("~");
 // ros::Subscriber subscriber = nodeHandle.subscribe("/scan", 50, scanCallback);
 // ros::Publisher publisher = nodeHandle.advertise<geometry_msgs::Twist>("/cmd_vel", 50);
+ros::Publisher publisher;
+
 void scanCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
 {
 
 
 double angle=0;
 double rangeMin=100000;
-for(double i=0;i<=720;i=i+1)
+for(double i=0;i<720;i=i+1)
 {
   if(msg->ranges[i] <= rangeMin)
   {
@@ -70,34 +72,40 @@ for(double i=0;i<=720;i=i+1)
     angle = i/2;
   }
 }
-ROS_INFO("Range: [%f]", msg->range_min);
+ROS_INFO("Range: [%f]", rangeMin);
 ROS_INFO("Angle: [%f]", angle);
-angle = angle*3.14/180;
-double xrel = rangeMin*cos(angle);
-double yrel = rangeMin*sin(angle);
-// ros::Rate loop_rate(10);
-// int count = 0;
-//   while (angle!=0)
-//   { geometry_msgs::Twist move;
-//     move.linear.x=10;
-//     move.angular.x=100;
-//     move.angular.y=100;
-//     move.angular.z=100;
-//     publisher.publish(move);
-//     ros::spinOnce();
-//     loop_rate.sleep();
-//     ++count;
-//   }
-//   count = 0;
-//   while (xrel>0.6)
-//   { geometry_msgs::Twist move;
-//     move.linear.x=10;
-//     publisher.publish(move);
-//     ros::spinOnce();
-//     loop_rate.sleep();
-//     ++count;
-//   }
+double angle2 = angle*3.14/180;
+double xrel = rangeMin*cos(angle2);
+double yrel = rangeMin*sin(angle2);
+ros::Rate loop_rate(10);
+int count = 0;
+  while (angle>10)
+  { geometry_msgs::Twist move;
+    move.linear.x=10;
+    // move.angular.x=100;
+    // move.angular.y=100;
+    move.angular.z=10;
+    publisher.publish(move);
+    ros::spinOnce();
+    loop_rate.sleep();
+    ++count;
+  }
+  geometry_msgs::Twist move;
+    move.linear.x=0;
+    move.angular.x=0;
+    move.angular.y=0;
+    move.angular.z=0;
+    publisher.publish(move);
 
+  count = 0;
+  while (xrel>0.6)
+  { geometry_msgs::Twist move;
+    move.linear.x=10;
+    publisher.publish(move);
+    ros::spinOnce();
+    loop_rate.sleep();
+    ++count;
+  }
 }
 // geometry_msgs::Twist move;
 // while(msg->range_min>0)
@@ -126,9 +134,9 @@ int main(int argc, char** argv) {
   ros::init(argc, argv, "smb_highlevel_controller");
   // controller cont;
   ros::NodeHandle nodeHandle("~");
-
+  publisher = nodeHandle.advertise<geometry_msgs::Twist>("/cmd_vel", 50);
   ros::Subscriber subscriber = nodeHandle.subscribe("/scan", 50, scanCallback);
-  ros::Publisher publisher = nodeHandle.advertise<geometry_msgs::Twist>("/cmd_vel", 50);
+  
   // ros::Rate loop_rate(10);
 
   // int count = 0;
